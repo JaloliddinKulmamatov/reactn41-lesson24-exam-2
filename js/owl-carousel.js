5e5d5a521ae6eabbf34dcb98236684451c795e03
JaloliddinKulmamatov <itjalol2010@gmail.com>
2023-12-06 15:05:02 +0500

    first commit

---
+++ js/owl-carousel.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $("#team-carousel").owlCarousel({
+    margin:20,
+    loop:true,
+    nav:true,
+    items:4,
+    // navText:['<img scr="../images/left.svg" />' , 
+    // '<img scr="../images/right.svg" />'],
+  })
+});
